6ee23be9cca0343134b5c954d82dd263344d327b
unknown <johnmoony@mail.ru>
2016-05-06 18:33:10 +0300

    Initial.

---
+++ Assets/Spin.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spin : MonoBehaviour {
+
+	public float speed = 10f;
+
+	public Vector3 direction = new Vector3(0,0,3);
+		
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(direction, speed * Time.deltaTime);
+	}
+}
