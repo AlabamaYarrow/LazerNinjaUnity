e6f1830852714a7af54e2aac84dea95c5676e541
unknown <johnmoony@mail.ru>
2016-05-14 18:00:21 +0300

    Shooting physics.

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shoot : MonoBehaviour {
+
+
+	public Rigidbody Bullet;
+	public float ShootForce;
+	public Transform ShootPosition;
+	public float verticalOffset;
+
+	private float delay = 2.0f; 
+
+	void Start () {
+		InvokeRepeating("ShootBullet", 1, 1);
+
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			ShootBullet();
+		}
+	}
+
+	void ShootBullet() {
+		Vector3 position = transform.position;
+		position.y = position.y + verticalOffset;
+		Rigidbody instanceBullet = (Rigidbody) Instantiate (Bullet, position, ShootPosition.rotation);
+		instanceBullet.GetComponent<Rigidbody>().AddForce (ShootPosition.forward * ShootForce);
+	}	
+}
