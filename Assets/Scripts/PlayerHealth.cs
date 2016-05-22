27c00dc7a512249340c93774acb86838718a7f85
Linus Torvalds <torvalds@linux-foundation.org>
2016-05-22 20:01:54 +0300

    Readme test.

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -9,22 +9,39 @@ public class PlayerHealth : MonoBehaviour {
 	public Text HealthText;
 	public int decreaseSpeed = 1;
 
+	GameObject GameoverTextLeft;
+	GameObject GameoverTextRight;
+
 	void Start () {
+		GameoverTextLeft = GameObject.Find("GameOverTextLeft");
+		GameoverTextRight = GameObject.Find("GameOverTextRight");
 	}
 
-	void Update () {
-	
+	void Update () {}
+
+	void SetHealth() {
+		HealthBar.value = health;
+		HealthText.text = health.ToString();
 	}
 
 	public void DecreaseHealth() 
 	{
 		if (health <= 0) {
+			var gameoverText = GameoverTextLeft.GetComponent<GameoverText> ();
+			gameoverText.Appear();
+			gameoverText = GameoverTextRight.GetComponent<GameoverText> ();
+			gameoverText.Appear();
+			health = 0;
+			SetHealth ();
 			return;
 		}
-		HealthBar.value = health;
-		HealthText.text = health.ToString();
+		SetHealth ();
 		health -= decreaseSpeed;
 	}
 
 
 }
+
+
+
+
