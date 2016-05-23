1393d43bbabb031ac21405a30f1f5d6f6d373291
BorisKolganov <bkolganov@me.com>
2016-05-23 19:44:44 +0300

    nice menu

---
+++ Assets/KeyBoard.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyBoard : MonoBehaviour {
+
+	// Use this for initialization
+	public InstantGuiInputText input;
+	private TouchScreenKeyboard keyboard;
+	void Start () {
+
+	}
+	void OnGUI (){
+		if (input.pressed) {
+			keyboard = TouchScreenKeyboard.Open("");
+		}
+		if(keyboard!=null) input.text = keyboard.text.Trim();
+	}
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ Assets/OnStartScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnStartScreen : MonoBehaviour {
+
+	// Use this for initialization
+	public InstantGuiInputText IP;
+	void Start () {
+		NetworkMngr.IP = IP.text;
+		Application.LoadLevel (2);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
