537e4c4801af5db9a78cfc531c829b123a45559c
BorisKolganov <bkolganov@me.com>
2016-08-10 22:25:47 +0300

    broadcast done

---
+++ Assets/Scripts/BroadcastListener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class BroadcastListener : NetworkDiscovery
+{
+
+	public override void OnReceivedBroadcast (string fromAddress, string data)
+	{
+		NetworkMngr.IP = fromAddress;
+		Application.LoadLevel (2);
+	}
+
+}
---
+++ Assets/Scripts/Listener.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Listener : MonoBehaviour {
+
+	// Use this for initialization
+	public BroadcastListener lstnr; 
+	void Start () {
+//		lstnr = new BroadcastListener ();
+		lstnr.Initialize();
+		lstnr.StartAsClient ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+		
+}
